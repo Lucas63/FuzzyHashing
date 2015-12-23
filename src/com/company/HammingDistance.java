81866a67920947e460b0c6750d9b7fcda162f689
lucas63 <lucas63@yandex.com>
2015-12-23 15:49:26 +0200

    INITIAL

---
+++ src/com/company/HammingDistance.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Created by lucas63 on 23.12.15.
+ */
+public class HammingDistance {
+    public static double distance(String s1,String s2)
+    {
+        double a = 0;
+        double b = 0;
+        for (int x = 0; x < s1.length(); x++) { //both are of the same length
+            if (s1.charAt(x) == s2.charAt(x)) {
+                a += 1;
+            }
+            b++;
+        }
+        return a/b;
+    }
+}
